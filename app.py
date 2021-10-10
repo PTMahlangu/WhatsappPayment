51fd2e0717d95486706081ef2f355e4cdedee856
PTMahlangu <mahlangupt.1010@gmail.com>
2021-10-10 15:37:16 +0200

    payment

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+client = Client(account_sid, auth_token)
+
+def respond(message):
+    response = MessagingResponse()
+    response.message(message)
+    return str(response)
+
+@app.route('/')
+def home():
+    return 'welcome to the whatsappbot'
+
+@app.route('/message', methods=['POST'])
+def reply():
+    message = request.form.get('Body').lower()
+    if message:
+        return respond(f'Thank you for your message! A member of our team will be in touch with you soon.')
