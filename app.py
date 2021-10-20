a7826b236ecdf467922b371368df34219eb736e1
PTMahlangu <mahlangupt.1010@gmail.com>
2021-10-20 10:35:25 +0200

    brian working

--- app.py
+++ app.py
@@ -1,49 +1,8 @@
-import os
-import aiml
-from dotenv import load_dotenv
-from flask import Flask, request, render_template
-from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
-from payment.paystack import initializePayment
 
-
-load_dotenv()
-
-# Create the kernel and learn AIML files
-kernel = aiml.Kernel()
-
-for filename in os.listdir("brain"):
-	if filename.endswith(".aiml"):
-		kernel.learn("brain/" + filename)
-
-
-# while True:
-#     msg =input("Enter input :")
-
-#     if not msg.isdigit():
-#         reply = kernel.respond(msg)
-#     else:
-#         reply = kernel.respond("Pay "+msg )
-
-    
-#     if kernel.getPredicate("amount"):
-#         amount = int(kernel.getPredicate("amount"))*100
-#         url = "https://www.tutorialspoint.com/aiml/aiml_setget_tag.htm"
-#         kernel.setPredicate("url",url)
-
-    # print(reply)
+from flask import Flask, request
+from controller import WhatsappRespond
 
 app = Flask(__name__)
-
-account_sid = os.environ['account_sid']
-auth_token = os.environ['auth_token']
-client = Client(account_sid, auth_token)
-
-
-def respond(message):
-    response = MessagingResponse()
-    response.message(message)
-    return str(response)
 
 @app.route('/')
 def home():
@@ -55,21 +14,7 @@ def reply():
     message = request.form.get('Body').lower()
     phone_no = request.form.get('From')
 
-
-    if not message.isdigit():
-        reply = kernel.respond(message)
-    else:
-        reply = kernel.respond("Pay "+message)
-
-    if not reply:
-        reply = "Oops! Something wrong. How much would you like to pay?"
-
-    if kernel.getPredicate("amount"):
-        amount = int(kernel.getPredicate("amount"))*100
-        url = initializePayment(phone_no,amount)["data"]["authorization_url"]
-        kernel.setPredicate("url",url)
-
-    return respond(reply)
+    WhatsappRespond(message,phone_no)
 
 
 
---
+++ controller.py
@@ -0,0 +1,43 @@
+import os
+import aiml
+from dotenv import load_dotenv
+from flask import Flask, request, render_template
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+from payment.paystack import initializePayment
+
+load_dotenv()
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+client = Client(account_sid, auth_token)
+
+# Create the kernel and learn AIML files
+kernel = aiml.Kernel()
+
+for filename in os.listdir("brain"):
+	if filename.endswith(".aiml"):
+		kernel.learn("brain/" + filename)
+
+def respond(message):
+    response = MessagingResponse()
+    response.message(message)
+    return str(response)
+
+
+def WhatsappRespond(message,phone_no):
+
+    if not message.isdigit():
+        reply = kernel.respond(message)
+    else:
+        reply = kernel.respond("Pay "+message)
+
+    if not reply:
+        reply = "Oops! Something wrong. How much would you like to pay?"
+
+    if kernel.getPredicate("amount"):
+        amount = int(kernel.getPredicate("amount"))*100
+        url = initializePayment(phone_no,amount)["data"]["authorization_url"]
+        kernel.setPredicate("url",url)
+
+    return respond(reply)
