b1f2070551114b33dc2d1f997321a6b8f0c4f28f
PTMahlangu <mahlangupt.1010@gmail.com>
2021-10-22 12:18:08 +0200

    semi working yoco

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import aiml
+import json
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
@@ -7,12 +8,32 @@ from twilio.rest import Client
 from payment.yoco import yocoPayment
 
 
+load_dotenv()
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+client = Client(account_sid, auth_token)
+kernel = aiml.Kernel()
+
+for filename in os.listdir("brain"):
+	if filename.endswith(".aiml"):
+		kernel.learn("brain/" + filename)
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
+def respond(message):
+    response = MessagingResponse()
+    response.message(message)
+    return str(response)
+
+
+@app.route('/yoco')
 def home():
-    return render_template('yoco.html')
+    user_amount = 6200
+    print('url:' +str(request.url_root)+"yoco")
+    return render_template('yoco.html',data=json.dumps(user_amount))
 
 
 @app.route('/pay', methods=['POST'])
