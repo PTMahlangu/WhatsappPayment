70b77245eb6a950b8b9265f8d62a1c4dd9ab9972
PTMahlangu <mahlangupt.1010@gmail.com>
2021-10-25 17:55:43 +0200

    working

--- app.py
+++ app.py
@@ -39,21 +39,21 @@ def home():
 
 @app.route('/pay')
 def yoco():
-    global user_amount
-    user_amount = 5200
+    # global user_amount
+    # user_amount = 5200
     return render_template('yoco.html',data=json.dumps(user_amount))
 
 
 @app.route('/yoco', methods=['POST'])
 def yoco_pay():
-    global user_amount
+    # global user_amount
     token = request.get_json()
     return yocoPayment(token,user_amount)
 
 
 @app.route('/message', methods=['POST'])
 def reply():
-    # global user_amount
+    global user_amount
     message = request.form.get('Body').lower()
     phone_no = request.form.get('From')
 
